30a6cad3129f3cdd164dbc955b0e73f71afb4342
veyner <myfun239@mail.ru>
2018-05-28 22:16:09 +0300

    быки и коровы v0.1 #2

---
+++ BullNCow/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Humanizer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Humanizer.Inflections;
+
+namespace BullNCow
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ru-RU");
+            Vocabularies.Default.AddIrregular("корова", "коровы");
+            Vocabularies.Default.AddIrregular("бык", "быка");
+            Console.WriteLine("Загадайте 4-значное число, состоящее из неповторяющихся цифр");
+            var firstNumber = Console.ReadLine();
+            Console.Clear();
+            var bull = 0;
+            var cow = 0;
+            var score = 0;
+            Console.WriteLine("Необходимо угадать загаданное число за ограниченное число ходов, вводя 4-значные числа");
+            for (; bull < 4;)
+            {
+                var secondNumber = Console.ReadLine();
+                for (int i = 0; i < secondNumber.Length; i++)
+                {
+                    var k = firstNumber.IndexOf(secondNumber[i]);
+                    if (k == i)
+                    {
+                        bull++;
+                    }
+                    else if (k != -1)
+                    {
+                        cow++;
+                    }
+                }
+                score++;
+                Console.WriteLine("Бык".ToQuantity(bull));
+                Console.WriteLine("Корова".ToQuantity(cow));
+
+                if (bull == 4)
+                {
+                    Console.WriteLine("Вы угадали число за {0} ходов", score);
+                    break;
+                }
+                cow = 0;
+                bull = 0;
+            }
+            Console.ReadLine();
+        }
+    }
+}
